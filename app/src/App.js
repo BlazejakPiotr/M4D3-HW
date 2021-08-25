c0e657b23373fec49caece3b37c075b2e3bdf7c5
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-25 16:01:54 +0200

    fixing git

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import WarningSign from "./components/WarningSign";
-import MyBadge from "./components/MyBadge";
 
 function App() {
-  return (
-    <div className="App">
-      <WarningSign message="Hello, this is working" />
-      <MyBadge text="Strive Badge" color="secondary" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
