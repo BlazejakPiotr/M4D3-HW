c4ec1ac723729a823dbad3b641f48b7a4e621871
js25s <petrauskas.justas@gmail.com>
2021-08-25 14:33:20 +0200

    1st is done

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,12 @@
-// import { WarningSign } from "WarningSign";
+import "bootstrap/dist/css/bootstrap.min.css";
+import WarningSign from "./components/WarningSign";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <WarningSign message="Hello, this is working" />
+    </div>
+  );
 }
 
 export default App;
