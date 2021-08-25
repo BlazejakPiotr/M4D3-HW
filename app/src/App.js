44c21d77de11227a11c98ed677d2b0bab9ef7c7e
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-25 14:47:17 +0200

    badge

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import WarningSign from "./components/WarningSign";
+import MyBadge from "./components/MyBadge";
 
 function App() {
   return (
     <div className="App">
       <WarningSign message="Hello, this is working" />
+      <MyBadge text="Strive Badge" color="primary" />
     </div>
   );
 }
